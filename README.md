eda608186397d5986397dd06581b5836adf0fbf4
Mike Conley <mike.d.conley@gmail.com>
2018-08-23 16:19:52 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# INXPO Speed Control
+
+The new AirMozilla uses INXPO / onlineexperiences.com as our video host. Over the years, I've gotten used to watching pre-recorded AirMozilla videos at > 1.0x speed, but unfortunately, the INXPO video player doesn't expose the ability to control playback rate.
+
+This WebExtension fixes that. Only tested on https://airmo.mozilla.org, and only on a few videos.
---
+++ content.js
@@ -0,0 +1,52 @@
+const SpeedControl = {
+  SPEEDS: {
+    "1.0x": 1,
+    "1.25x": 1.25,
+    "1.5x": 1.5,
+    "2.0x": 2,
+  },
+
+  init() {
+    this.$video = document.querySelector(".AxiomVideoHTML");
+    this.$controls = document.querySelector(".AxiomMediaBottomControls");
+
+    if (!this.$video || !this.$controls) {
+      // I guess this isn't the right kind of page.
+      console.log("INXPO Speed Control can't attach to this page. Bailing out.");
+    }
+
+    this.$select = document.createElement("select");
+    this.$select.addEventListener("change", this);
+
+    let frag = document.createDocumentFragment();
+
+    for (let speedStr in this.SPEEDS) {
+      let option = document.createElement("option");
+      option.textContent = speedStr;
+      option.value = this.SPEEDS[speedStr];
+      frag.appendChild(option);
+    }
+
+    // Gettin' lazy
+    this.$select.style.marginLeft = "5px";
+    this.$select.style.marginRight = "5px";
+
+    this.$select.appendChild(frag);
+    this.$controls.appendChild(this.$select);
+  },
+
+  handleEvent(event) {
+    if (event.type != "change") {
+      return;
+    }
+
+    let newSpeed = event.target.value;
+    console.log(`Setting playbackRate to ${newSpeed}`);
+    this.$video.playbackRate = newSpeed;
+  },
+}
+
+addEventListener("load", () => {
+  console.log("Starting up INXPO Speed Control.");
+  SpeedControl.init();
+}, { once: true });
